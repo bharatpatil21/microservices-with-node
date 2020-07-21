0015f242820cfcef945e01c750f7319fc7e82073
bharat_patil <patilbharat09@gmail.com>
2020-07-21 11:01:13 +0530

    Added create comment and comment list component

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
+
 export default () => {
   const [posts, setPosts] = useState({});
 
@@ -24,6 +27,8 @@ export default () => {
         <div className="card-body">
           <h3>{post.title}</h3>
         </div>
+        <CommentList postId={post.id} />
+        <CommentCreate postId={post.id} />
       </div>
     );
   });
