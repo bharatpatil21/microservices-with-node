52fe15d11fa0b59494d4897296667fbcc899e453
bharat_patil <patilbharat09@gmail.com>
2020-08-16 10:17:14 +0530

    Updated comment status by using moderate service

--- events-bus/index.js
+++ events-bus/index.js
@@ -5,13 +5,13 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-const posts = {};
-
 app.post("/events", (req, res) => {
   const event = req.body;
+
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
   axios.post("http://localhost:4002/events", event);
+  axios.post("http://localhost:4003/events", event);
 
   res.send({ status: "OK" });
 });
--- query/index.js
+++ query/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const cors = require("cors");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
-//
 
 app.get("/posts", (req, res) => {
   res.send(posts);
@@ -18,15 +17,30 @@ app.post("/events", (req, res) => {
 
   if (type === "PostCreated") {
     const { id, title } = data;
+
     posts[id] = { id, title, comments: [] };
   }
 
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
+  console.log(posts);
 
   res.send({});
 });
